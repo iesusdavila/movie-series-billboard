f9be414b9d3c732ff9df234c39550fb730bc6917
iesusdavila <irdavila@espol.edu.ec>
2022-09-22 12:41:47 -0500

    Diseño e implementacion de las peliculas trending

--- src/main.js
+++ src/main.js
@@ -9,6 +9,20 @@ const api = axios.create({
   },
 });
 
+const arrowLeft = document.querySelector(".arrowLeft");
+const arrowRight = document.querySelector(".arrowRight");
+const trendingPreview = document.querySelector(".trendingPreview-movieList");
+const trendingCarousel = document.querySelector(".trendingCarousel");
+const movieContainer = document.querySelectorAll(
+  ".trendingPreview-movieList .movie-container"
+);
+
+const movieContainerWidth = movieContainer[0].offsetWidth;
+console.log(movieContainer);
+
+arrowLeft.onclick = () => Move(1);
+arrowRight.onclick = () => Move(2);
+
 async function getCategoriesMovies() {
   const { data } = await api("genre/movie/list");
 
@@ -36,4 +50,70 @@ async function getCategoriesMovies() {
   });
 }
 
+async function getTrendingMoviesPreview() {
+  const { data } = await api("trending/movie/day");
+
+  const movies = data.results;
+
+  const trendingPreview_movieList = document.querySelector(
+    ".trendingPreview-movieList"
+  );
+
+  trendingPreview_movieList.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const movie_container = document.createElement("div");
+    movie_container.classList.add("movie-container");
+    movie_container.addEventListener("click", () => {
+      location.hash = "#movie=" + movie.id;
+    });
+
+    const imageMovie = document.createElement("img");
+    imageMovie.classList.add("movie-img");
+    imageMovie.alt = movie.title;
+    imageMovie.setAttribute(
+      "src",
+      "https://image.tmdb.org/t/p/w300/" + movie.poster_path
+    );
+
+    // observer.observe(imageMovie);
+
+    movie_container.appendChild(imageMovie);
+    trendingPreview_movieList.appendChild(movie_container);
+  });
+}
+
+function Move(value) {
+  const trendingPreviewWidth = trendingPreview.offsetWidth;
+  const trendingCarouselWidth = trendingCarousel.offsetWidth;
+
+  trendingPreview.style.left == ""
+    ? (leftPosition = trendingPreview.style.left = 0)
+    : (leftPosition = parseFloat(trendingPreview.style.left.slice(0, -2) * -1));
+
+  console.log(trendingPreview.style.left.slice(0, -2) * -1);
+  console.log(leftPosition);
+  console.log({
+    trendingPreviewWidth,
+    trendingCarouselWidth,
+  });
+
+  const rightLimit =
+    leftPosition < trendingPreviewWidth - trendingCarouselWidth;
+  const leftLimit = leftPosition > 0;
+
+  if (rightLimit && value == 2) {
+    trendingPreview.style.left = `${
+      -1 * (leftPosition + movieContainerWidth)
+    }px`;
+  } else if (leftLimit && value == 1) {
+    trendingPreview.style.left = `${
+      -1 * (leftPosition - movieContainerWidth)
+    }px`;
+  } else if (!rightLimit) {
+    console.log("Borrar la flecha");
+  }
+}
+
 getCategoriesMovies();
+getTrendingMoviesPreview();
